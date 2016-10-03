f6e53074f22c73c734dd30810eddca5e8cdabd45
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-10-03 23:00:56 +0100

    more tests + dealership added

--- car_class.rb
+++ car_class.rb
@@ -1,17 +1,44 @@
 class Car
 
-  attr_reader(:fuel_level, :speed)
+  attr_accessor(:fuel_level, :speed)
 
-  def initialize
+  @@dealership = "Kyle's Dodgy Cars"
+
+
+  def initialize(fuel_level, speed)
     @fuel_level = 100
     @speed = 0
   end
 
-def accelerate
-  @speed += 10
-  @fuel_level -= 5
-end
 
+  def accelerate
+    @speed += 10
+    @fuel_level -= 5
+  end
+
+
+  def break
+    if @speed <= 10
+      @speed = 0
+    else
+      @speed -= 10
+    end
+  end
+
+
+  def fuel_level
+    return @fuel_level
+  end
+
+
+  def speed_level
+    return @speed
+  end
+
+
+  def self.dealership
+    return @@dealership
+  end
 
 
 end
--- specs/car_class_spec.rb
+++ specs/car_class_spec.rb
@@ -4,13 +4,52 @@ require_relative('../car_class')
 
 class TestCar < MiniTest::Test
 
-  def setup
-    @hyundai = Car.new()
-    @mitsubishi = Car.new()
-    @suzuki = Car.new()
-    @daihatsu = Car.new()
-end
 
+  def setup
+    @mitsubishi = Car.new(100, 0)
+    @suzuki = Car.new(100, 0)
+  end
+
+
+  def test_fuel_level_mitsubishi
+    assert_equal(100, @mitsubishi.fuel_level)
+  end
+
+
+  def test_speed_level_suzuki
+    assert_equal(0, @suzuki.speed)
+  end
+
+
+  def test_accelerate_suzuki
+    @suzuki.accelerate
+    assert_equal(10, @suzuki.speed)
+    assert_equal(95, @suzuki.fuel_level)
+  end
+
+
+  def test_accelerate_mitsubishi
+    @mitsubishi.accelerate
+    assert_equal(10, @mitsubishi.speed)
+    assert_equal(95, @mitsubishi.fuel_level)
+  end
+
+
+  def test_break_suzuki
+    @suzuki.break
+    assert_equal(0, @suzuki.speed)
+  end
+
+
+  def test_break_mitsubishi
+    @mitsubishi.break
+    assert_equal(0, @mitsubishi.speed)
+  end
+
+  def test_dealership
+    name = Car.dealership
+    assert_equal("Kyle's Dodgy Cars", name)
+  end
 
 
 end
